4998871d7cc3466901543bc66320c085022ce52a
ongunuzaymacar <oum2000@columbia.edu>
2019-07-13 05:24:49 +0300

    Added local-p* approach to text generation example.

--- examples/text_generation.py
+++ examples/text_generation.py
@@ -8,9 +8,9 @@ Script containing an example and guideline for training multi-class language mod
 generation, using the implemented @Attention and @SelfAttention() layers.
 
 Usage:
-  # Train a BiLSTM model:
+  # Train a LSTM model:
   python text_generation.py
-  # Train BiLSTM model with non-penalized self-attention:
+  # Train LSTM model with non-penalized self-attention:
   python text_generation.py --config=1
   # Check CONFIG OPTIONS comment block for more options
 """
@@ -22,8 +22,8 @@ import tensorflow as tf
 from tensorflow.keras.utils import get_file, to_categorical
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.metrics import categorical_accuracy
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Layer, Lambda, Input, Embedding, Bidirectional, Dense
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Layer, Lambda, Input, Embedding, Bidirectional, Flatten, Dense
 from tensorflow.compat.v1.keras.layers import CuDNNLSTM   # CuDNNLSTM not yet released for TF 2.0
 
 import sys
@@ -36,13 +36,10 @@ parser.add_argument("--config",
                     default=0,
                     help="Integer value representing a model configuration")
 # CONFIG OPTIONS:
-# 0: BiLSTM Model
-# 1: BiLSTM Model w/ Self-Attention (Non-Penalized)
-# 2: BiLSTM Model w/ Self-Attention (Penalized)
-# 3: BiLSTM Model w/ Global Attention
-# 4: BiLSTM Model w/ Local-m Attention
-# 5: BiLSTM Model w/ Local-p Attention
-# 6: BiLSTM Model w/ Local-p* Attention
+# 0: LSTM Model
+# 1: LSTM Model w/ Self-Attention (Non-Penalized)
+# 2: LSTM Model w/ Global Attention
+# 3: LSTM Model w/ Local-p* Attention
 args = parser.parse_args()
 
 # Set seeds for reproducibility
@@ -50,10 +47,10 @@ np.random.seed(500)
 tf.random.set_seed(500)
 
 # Set global constants
-sequence_length = 50    # choose 50-character sequences
-embedding_dims = 256    # number of dimensions to represent each character in vector space
+sequence_length = 120   # choose 120-character sequences
+embedding_dims = 50     # number of dimensions to represent each word in vector space
 batch_size = 100        # feed in the neural network in 100-example training batches
-num_epochs = 10         # number of times the neural network goes over EACH training example
+num_epochs = 15         # number of times the neural network goes over EACH training example
 config = int(args.config)  # model-configuration
 
 # Load Shakespeare corpus file (.txt)
@@ -64,16 +61,19 @@ file = get_file(
 
 # Extract data (string format) from corpus file
 corpus = open(file=file, mode='rb').read().decode(encoding='utf-8')
+# Optionally trim data for memory storage and performance purposes
+# corpus = corpus[:len(corpus) // 2]
 
 # Set corpus-specific attributes for language modelling
-vocabulary = sorted(set(corpus))  # char-level vocabulary
+# corpus = corpus.split()  # uncomment to convert to word-level
+vocabulary = sorted(set(corpus))  # word-level vocabulary
 vocabulary_size = len(vocabulary)
 char_to_index = {char: index for index, char in enumerate(vocabulary)}
 
-# Encode each sequence using char to index mapping
+# Encode each sequence using word to index mapping
 sequences = []
 for i in range(0, len(corpus) - sequence_length, 1):
-    sequences.append([char_to_index[char] for char in corpus[i: i+sequence_length]])
+    sequences.append([char_to_index[word] for word in corpus[i: i+sequence_length]])
 
 # Set training inputs and labels
 m = len(sequences)  # number of training examples
@@ -111,29 +111,42 @@ def perplexity(labels, logits):
     return pow(2, loss(y_true=labels, y_pred=logits))
 
 
-# Create multi-class, character-level text generation model
-model = Sequential()
+# Create multi-class, char-level text generation model
 # Input Layer
-model.add(Input(shape=(sequence_length,), batch_size=batch_size))
-# Character-Embedding Layer
-model.add(Embedding(input_dim=vocabulary_size, output_dim=embedding_dims))
+X = Input(shape=(sequence_length,), batch_size=batch_size)
+
+# Word-Embedding Layer
+embedded = Embedding(input_dim=vocabulary_size, output_dim=embedding_dims)(X)
+
 # Recurrent Layer
-model.add(Bidirectional(CuDNNLSTM(units=512, return_sequences=True if config != 0 else False)))
+if config != 0:
+    encoder_output, hidden_state, cell_state = CuDNNLSTM(units=512,
+                                                         return_sequences=True,
+                                                         return_state=True)(embedded)
+    attention_input = [encoder_output, hidden_state]
+else:
+    encoder_output = CuDNNLSTM(units=512)(embedded)
+
 # Optional Attention Mechanisms
 if config == 1:
-    model.add(SelfAttention(size=500, num_hops=8, use_penalization=False))
+    encoder_output, attention_weights = SelfAttention(size=50,
+                                                      num_hops=16,
+                                                      use_penalization=False)(encoder_output)
 elif config == 2:
-    model.add(Attention(size=500, context='many-to-one', alignment_type='global'))
+    encoder_output, attention_weights = Attention(context='many-to-one',
+                                                  alignment_type='global')(attention_input)
+    encoder_output = Flatten()(encoder_output)
 elif config == 3:
-    model.add(Attention(size=500, context='many-to-one', alignment_type='local-m'))
-elif config == 4:
-    model.add(Attention(size=500, context='many-to-one', alignment_type='local-p'))
-elif config == 5:
-    model.add(Attention(size=500, context='many-to-one', alignment_type='local-p*'))
+    encoder_output, attention_weights = Attention(context='many-to-one',
+                                                  alignment_type='local-p*',
+                                                  window_width=25)(attention_input)
+    encoder_output = Flatten()(encoder_output)
+
 # Prediction Layer
-model.add(Dense(units=vocabulary_size, activation='softmax'))
+Y = Dense(units=vocabulary_size, activation='softmax')(encoder_output)
 
 # Compile model
+model = Model(inputs=X, outputs=Y)
 model.compile(loss=loss, optimizer='adam', metrics=[perplexity, categorical_accuracy])
 print(model.summary())
 
